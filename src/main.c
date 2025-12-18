3af640755e10f14c4eb7ef3373036d78f9c12020
dgullient <gullientdowan35@gmail.com>
2025-12-18 16:14:10 +0100

    debug

--- src/main.c
+++ src/main.c
@@ -55,13 +55,6 @@ int main(void) {
 
     BoardMppt_LED_Init();
 
-	for (int i = 0 ; i < 3 ; i = i + 1) {
-		BoardMppt_LED_On();
-		HAL_Delay(500);
-		BoardMppt_LED_Off();
-		HAL_Delay(500);
-	}
-
 
     /* Initialisation des périphériques */
     MX_DMA_Init();
@@ -69,12 +62,6 @@ int main(void) {
     MX_TIM1_PWM_Init();
     MX_USART1_UART_Init();
 
-    	for (int i = 0 ; i < 3 ; i = i + 1) {
-		BoardMppt_LED_On();
-		HAL_Delay(500);
-		BoardMppt_LED_Off();
-		HAL_Delay(500);
-	}
 
     /* Démarrage de la calibration de l'ADC */
     if (HAL_ADCEx_Calibration_Start(&hadc1) != HAL_OK)
